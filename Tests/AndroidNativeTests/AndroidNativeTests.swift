f76c21649652bbabe621b20c6ceb09304729d959
Marc Prud'hommeaux <marc@skip.tools>
2024-12-07 16:13:42 -0500

    Add testMainActor

--- Tests/AndroidNativeTests/AndroidNativeTests.swift
+++ Tests/AndroidNativeTests/AndroidNativeTests.swift
@@ -2,7 +2,37 @@ import XCTest
 import AndroidNative
 
 class AndroidNativeTests : XCTestCase {
-    public func testAndroidNative() {
-        XCTAssertEqual(1 + 2, 3)
+    public func testMainActor() async {
+        let actorDemo = await MainActorDemo()
+        let result = await actorDemo.add(n1: 1, n2: 2)
+        XCTAssertEqual(result, 3)
+        var tasks: [Task<Int, Never>] = []
+
+        for i in 0..<100 {
+            tasks.append(Task(priority: [.low, .medium, .high].randomElement()!) {
+                //print("### Task: \(Thread.current)")
+                assert(!Thread.isMainThread)
+                return await actorDemo.add(n1: i, n2: i)
+            })
+        }
+
+        var totalResult = 0
+        for task in tasks {
+            let taskResult = await task.value
+            totalResult += taskResult
+        }
+
+        XCTAssertEqual(9900, totalResult)
+    }
+}
+
+@MainActor class MainActorDemo {
+    init() {
+    }
+
+    func add(n1: Int, n2: Int) -> Int {
+        //print("### MainActorDemo: \(Thread.current)")
+        assert(Thread.isMainThread)
+        return n1 + n2
     }
 }
