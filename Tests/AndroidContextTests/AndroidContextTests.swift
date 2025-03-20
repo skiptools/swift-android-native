80a440b6a6867b7de82b0557b3a755163585a847
Marc Prud'hommeaux <marc@skip.tools>
2025-03-20 14:53:15 -0400

    Fix conditional import

--- Tests/AndroidContextTests/AndroidContextTests.swift
+++ Tests/AndroidContextTests/AndroidContextTests.swift
@@ -1,17 +1,20 @@
 import XCTest
 import AndroidContext
 import SwiftJNI
-#if canImport(AndroidNDK)
+#if os(Android)
 import AndroidNDK
 #endif
 
+@available(iOS 14.0, *)
 class AndroidContextTests : XCTestCase {
     public func testAndroidContext() throws {
-        #if canImport(AndroidNDK)
+        #if os(Android)
         throw XCTSkip("this test is only for demo purposes")
         let nativeActivity: ANativeActivity! = nil
         AndroidContext.contextPointer = nativeActivity.clazz
         let context = try AndroidContext.application
+        #else
+        throw XCTSkip("this test only exists for Android targets")
         #endif
 
     }
